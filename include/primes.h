7fcd964184e6dd9436b35fac3b6d6e74d38d83b8
Jeff Mariconda <jmariconda24@gmail.com>
2015-09-15 19:44:00 -0400

    update primes.c

--- include/primes.h
+++ include/primes.h
@@ -1,10 +1,13 @@
 #ifndef _PRIMES_H_
 #define _PRIMES_H_
+
 #ifndef NULL
 #define NULL ((void*)0)
 #endif
-#include <unistd.h>
+
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /*
   Author: Jeff Mariconda
@@ -12,5 +15,8 @@
 
   Program that uses child processes to find all of the primes in various ranges of numbers.
 */
+
+//Finds all primes in the given range (inclusive)
+int *find_primes(int start, int end);
 
 #endif
--- src/primes/primes.c
+++ src/primes/primes.c
@@ -1,5 +1,30 @@
 #include "primes.h"
 
+/*
+ *
+ *
+ */
+int *find_primes(int start, int end) {
+    return NULL;
+}
+
 int main(int argc, char **argv) {
-    
+    int pid;                        /* pid of the parent/child processes */
+    int nums[argc - 1];             /* array to hold the integer command line args */
+    int i;
+
+    /* check if enough command line args have been supplied and exit on error if not */
+    if (argc <= 1) {
+        printf("usage: ./primes <increasing positive integers>\n");
+        exit(-1);
+    }
+
+    /* get the integers that the user entered */
+    for (i = 1 ; i < argc ; i++) {
+        nums[i - 1] = atoi(argv[i]);
+    }
+
+
+
+    return 0;
 }
